f7cf6a9357d8bbd6bc54df7247f722911796dbb4
Edwin Molina Grisales <emolinagrisales@gmail.com>
2026-06-22 22:47:23 -0500

    Agregando configuración de lectura de puertos y carpeta dónde se guarda los datos puros de la telemetría

--- GetDataTelemetryUDP.py
+++ GetDataTelemetryUDP.py
@@ -1,11 +1,24 @@
+# Este script se encarga de leer los datos puros desde una fuente y guardarlos en un archivo
+# Siempre debe estar corriendo este script para obtener los datos
+
+import json
 import socket
 import datetime
 import os
 
 # --- CONFIGURACIÓN ---
-IP_ESCUCHA = "127.0.0.1"
-PUERTO_UDP = 20777
-CARPETA_LOGS = "DataUDPF12021"
+# IP_ESCUCHA = "127.0.0.1"
+# PUERTO_UDP = 20777
+# CARPETA_LOGS = "DataUDPF12021"
+
+# Obteniendo datos de configuración
+with open('./config.json', 'r') as file:
+    config = json.load(file)
+
+#Obteniendo los datos desde dónde se obtiene los datos de la telemetría desde la fuente (f1 2021, f1 2025, iracing, etc )
+IP_ESCUCHA   = config[ 'rawData' ][ 'ip' ]
+PUERTO_UDP   = config[ 'rawData' ][ 'portUdp' ]
+CARPETA_LOGS = config[ 'rawData' ][ 'folder' ]
 
 # Crear la carpeta si no existe
 if not os.path.exists(CARPETA_LOGS):
